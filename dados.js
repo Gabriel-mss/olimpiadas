d802fa9159b7549eb2b7341051eb6f43638d9ec6
Gabriel Martins <81273042+Gabriel-mss@users.noreply.github.com>
2024-09-06 22:48:21 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+  {
+    titulo: "Rayssa Leal",
+    descricao:
+      "Rayssa Leal, também conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com sua habilidade e carisma.",
+    link: "https://pt.wikipedia.org/wiki.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8"
+  },
+  {
+    titulo: "Beatriz Souza",
+    descricao:
+      "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação nas competições. Beatriz conquistou a primeira medalha de ouro do Brasil nas Olimpíadas de 2024 em Paris",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judô judo judoka"
+  },
+  {
+    titulo: "Rebeca Andrade",
+    descricao:
+      "Rebeca Andrade é uma ginasta brasileira que conquistou diversas medalhas em competições internacionais, sendo um dos maiores nomes da ginástica artística do país.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginástica gisnastica ouro"
+  },
+];
